f1d3d0b8f48b4b9b0a04dadaeda0af58e50fd12d
Alexander K. Hudek <alexander@hudek.org>
2014-04-22 16:11:50 -0400

    Actually pass the correct old state this time.

--- src/om/core.cljs
+++ src/om/core.cljs
@@ -210,7 +210,7 @@
            (if (satisfies? IShouldUpdate c)
              (should-update c
                (get-props #js {:props next-props})
-               state)
+               (aget state "__om_state"))
              (cond
                (not (identical? (-value (aget props "__om_cursor"))
                                 (-value (aget next-props "__om_cursor"))))
